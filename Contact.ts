a5719052db01ba93e3eaa07970ffa9b02920c05e
aakashvishu120 <aakashvishu120@gmail.com>
2025-06-17 13:01:48 +0530

    [Aakash]Add : Create a Contact in Address Book

---
+++ Contact.ts
@@ -0,0 +1,22 @@
+export class Contact {
+    constructor(
+        public firstname : string, 
+        public lastname : string, 
+        public address : string, 
+        public city : string, 
+        public state : string, 
+        public zip : string, 
+        public phone : string, 
+        public email : string
+    ){}
+
+    displayContact(){
+        console.log(`
+            Name : ${this.firstname} ${this.lastname}
+            Address : ${this.address} ${this.city} ${this.state} ${this.zip}             
+            Phone : ${this.phone}             
+            Email : ${this.email}
+        `);
+        
+    }
+}
--- addressBook.ts
+++ addressBook.ts
@@ -1,13 +1,64 @@
+import { Contact } from "./Contact";
+import * as readline from "readline";
+
+
 class AddressBookMain {
+
+    //To store all contacts
+    private contacts: Contact[] = []
+
     constructor() {
         this.displayAddressBook();
     }
 
-    displayAddressBook() {
+    private displayAddressBook(): void {
         console.log("Welcome to Address Book Program");
+        this.addContact();
     }
 
+    private rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    addContact(): void {
+        const questions: string[] = [
+            "First Name : ",
+            "Last Name : ",
+            "Address : ",
+            "City : ",
+            "State : ",
+            "Zip : ",
+            "Phone :",
+            "Email : "
+        ]
+
+        //To store all field of a single contact
+        const answers: string[] = []
+
+        const ask = (index: number) => {
+            if (index === questions.length) {
+                //array destructuring
+                const [firstname, lastname, address, city, state, zip, phone, email] = answers;
+
+                //creating a contact
+                const contact = new Contact(firstname, lastname, address, city, state, zip, phone, email);
+                this.contacts.push(contact);
+                console.log("contact added succesfully")
+                contact.displayContact();
+                this.rl.close();
+                return;
+            }
+
+            this.rl.question(questions[index], (input) => {
+                answers.push(input.trim());
+                ask(index + 1);
+            });
+        };
+        ask(0);
+    }
 }
+
 
 //To Display the welcome message
 const addressBook = new AddressBookMain();
