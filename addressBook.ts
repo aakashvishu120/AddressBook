eae788684a7fe16a541e7b999960fff3000c92b7
aakashvishu120 <aakashvishu120@gmail.com>
2025-06-17 11:32:09 +0530

    [Aakash]Add : Added new class AddressBookMain and Display welcome Message

---
+++ addressBook.ts
@@ -0,0 +1,13 @@
+class AddressBookMain {
+    constructor() {
+        this.displayAddressBook();
+    }
+
+    displayAddressBook() {
+        console.log("Welcome to Address Book Program");
+    }
+
+}
+
+//To Display the welcome message
+const addressBook = new AddressBookMain();
